d1e0f7ac9278b11a9ab17aff106ff52e3ca4354e
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-12-16 11:02:09 +0530

    This is the driver file for credit card assignment

--- src/Assignment2/driver.py
+++ src/Assignment2/driver.py
@@ -1,32 +1,34 @@
-import logging
-from pyspark.sql import SparkSession
+# driver.py
+
 from src.Assignment2.util import *
 
-logging.basicConfig(
-    level=logging.INFO, 
-    format="%(message)s"
-)
-logger = logging.getLogger()
-
-spark = SparkSession.builder.appName("CreditCardProcessing").getOrCreate()
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 
-data = [("1234567891234567",),
-        ("5678912345671234",),
-        ("9123456712345678",),
-        ("1234567812341122",),
-        ("1234567812341342",)]
-credit_card_df = create_credit_card_dataframe(spark, data)
-logger.info("Number of partitions before manipulation: %d", credit_card_df.rdd.getNumPartitions())
+spark = SparkSession.builder.appName("Assignment2).getOrCreate()
 
-credit_card_df = increase_partitions(credit_card_df, 5)
-logger.info("Number of partitions after increasing: %d", credit_card_df.rdd.getNumPartitions())
+input_path = "C:/Users/sravan/Documents/Book1.csv"
 
-credit_card_df = decrease_partitions(credit_card_df, 1)
-logger.info("Number of partitions after decreasing: %d", credit_card_df.rdd.getNumPartitions())
+#Read the CSV file into a DataFrame
+credit_card_df = read_csv(spark, input_path)
+log_partitions(credit_card_df, "Number of partitions before processing")
 
-mask_credit_card = mask_credit_card_udf()
-credit_card_df = credit_card_df.withColumn("masked_card_number", mask_credit_card("card_number"))
-credit_card_df.show(truncate=False)
+# Repartition the DataFrame to increase the partition size to 5
+increase_credit_card_df = increase_partitions(credit_card_df, 5)
+log_partitions(increase_credit_card_df, "Number of partitions after increasing")
 
+# Assuming the original number of partitions was stored in a variable 'original_partitions'
+back_to_og_credit_card_df = reduce_partitions(increase_credit_card_df, credit_card_df.rdd.getNumPartitions())
+log_partitions(back_to_og_credit_card_df, "Number of partitions after reducing")
 
+# Save DataFrame to disk in Parquet format
+credit_card_df.write.parquet("C:/Users/sravan/Documents/Book1.csv/Question2", mode="overwrite")
+
+# Read the saved Parquet file back to in-memory as DataFrame
+credit_card_df_from_disk = spark.read.parquet("C:/Users/sravan/Documents/Book1.csv/Question2")
+
+display(credit_card_df_from_disk)
+
+# Apply the UDF to create a new column 'masked_card_number'
+credit_card_df_with_masked = apply_mask_udf(credit_card_df)
+credit_card_df_with_masked.show()
